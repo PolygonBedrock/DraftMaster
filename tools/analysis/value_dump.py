200bade6360e7f9cb279e2fc88b147aa78ad839d
TwistedPolygon <cws12@my.fsu.edu>
2019-04-27 12:11:30 -0400

    value dump

---
+++ tools/analysis/value_dump.py
@@ -0,0 +1,21 @@
+from openers.value_data import *
+
+print("HERO VALUES")
+for hero in hero_values:
+    print(hero + "\t" + hero_values[hero])
+print("\n")
+
+print("MATCHUP VALUES")
+for matchup in matchup_values:
+    print(matchup + "\t" + matchup_values[matchup])
+print("\n")
+
+print("COMBO VALUES")
+for combo in combo_values:
+    print(combo + "\t" + combo_values[combo])
+print("\n")
+
+print("FACTION VALUES")
+for faction in faction_values:
+    print(faction + "\t" + faction_values[faction])
+print("\n")
